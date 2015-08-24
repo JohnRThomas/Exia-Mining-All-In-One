6eb5ce9ebed392e551832c25d0e8aa7acd9fd096
John <TheLimitInfinity@hotmail.com>
2015-08-24 01:40:22 -0400

    Added files for the mining guide location

---
+++ src/scripts/mining/locations/osrs/MiningGuild.java
@@ -0,0 +1,23 @@
+package scripts.mining.locations.osrs;
+
+public class MiningGuild extends OSRSLocation{
+
+	@Override
+	public void intialize(String ore) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getOres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/scripts/mining/locations/rs3/MiningGuild.java
@@ -0,0 +1,25 @@
+package scripts.mining.locations.rs3;
+
+import scripts.mining.locations.Location;
+
+public class MiningGuild extends Location{
+
+	@Override
+	public void intialize(String ore) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getOres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
