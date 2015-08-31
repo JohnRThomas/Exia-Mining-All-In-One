a1c28fa7017bd30e2467e479c77d407f7dfc9204
John <TheLimitInfinity@hotmail.com>
2015-08-31 17:38:17 -0400

    Removed exp from rock enum

--- src/scripts/mining/Rock.java
+++ src/scripts/mining/Rock.java
@@ -8,55 +8,31 @@ import javafx.collections.ObservableList;
 
 public enum Rock {
 
-	CLAY			("Clay", new String[]{"Clay"}, new double[]{5.0},
-			true, true, new Color(98,74,42), new Color(122,100,70)),
-	COPPER			("Copper", new String[]{"Copper ore"}, new double[]{17.5},
-			true, true, new Color(74,48,32)),
-	TIN				("Tin", new String[]{"Tin ore"}, new double[]{17.5},
-			true, true, new Color(106,106,106)),
-	LIMESTONE		("Limestone", new String[]{"Limestone"}, new double[]{26.5},
-			true, true, new Color(110,110,110)),
-	BLUERITE		("Bluerite", new String[]{"Bluerite ore"}, new double[]{17.5},
-			true, true, new Color(0,47,122)),
-	IRON			("Iron", new String[]{"Iron ore"}, new double[]{35.0},
-			true, true, new Color(32,17,14)),
-	//ELEMENTAL		("Elemental", new String[]{"Elemental ore"}, new double[]{0.0},
-	//		true, true, new Color(0,0,0)),
-	//DAEYALT			("Daeyalt", new String[]{"Daeyalt ore"}, new double[]{17.5},
-	//		true, false),
-	SILVER			("Silver", new String[]{"Silver ore"}, new double[]{40.0},
-			true, true, new Color(141,134,120), new Color(149,149,149)),
-	ESSENCE			("Essence", new String[]{"Rune essence", "Pure essence"}, new double[]{5.0, 5.0},
-			true, true),
-	COAL			("Coal", new String[]{"Coal"}, new double[]{50.0},
-			true, true, new Color(24,24,17), new Color(0,0,0)),
-	PAY_DIRT		("Ore vein", new String[]{"Pay-dirt"}, new double[]{60.0},
-			false, false),
-	SANDSTONE		("Sandstone", new String[]{"Sandstone (1kg)", "Sandstone (2kg)", "Sandstone (5kg)", "Sandstone (10kg)"}, new double[]{30.0, 40.0, 50.0, 60.0},
-			true, true, new Color(74,47,11)),
-	GEMS			("Gems", new String[]{"Uncut opal", "Uncut jade", "Uncut red topaz", "Uncut sapphire", "Uncut emerald", "Uncut diamond"}, new double[]{65.0, 65.0, 65.0, 65.0, 65.0, 65.0},
-			true, true, new Color(66,0,63)),
-	GOLD			("Gold", new String[]{"Gold ore"}, new double[]{65.0},
-			true, true, new Color(0,0,0)),
-	GRANITE			("Granite", new String[]{"Granite (500g)", "Granite (2kg)", "Granite (5kg)"}, new double[]{50.0, 60.0, 75.0},
-			true, true, new Color(94,66,40)),
-	MITHRIL			("Mithril", new String[]{"Mithril ore"}, new double[]{80.0},
-			true, true, new Color(47,47,66)),
-	ADAMANTITE		("Adamantite", new String[]{"Adamantite ore"}, new double[]{95.0},
-			true, true, new Color(52,60,52)),
-	BANE			("Bane", new String[]{"Bane ore"}, new double[]{90.0},
-			true, false),
-	LIVING_ROCK_REMAINS	("Living rock remains", new String[]{"Living minerals"}, new double[]{25.0},
-			true, false),
-	CONCENTRATED_COAL		("Concentrated Coal", new String[]{"Coal"}, new double[]{50.0},
-			true, false),
-	CONCENTRATED_GOLD		("Concentrated Gold", new String[]{"Gold ore"}, new double[]{65.0},
-			true, false),
-	RUNITE			("Runite", new String[]{"Runite ore"}, new double[]{125.0},
-			true, true, new Color(73,98,102)),
-	SEREN			("Seren", new String[]{"Corrupted ore"}, new double[]{296.7},
-			true, false),
-	UNKNOWN			("Unknown", new String[]{}, new double[]{0.0}, false, false);
+	CLAY				("Clay", new String[]{"Clay"},true, true, new Color(98,74,42), new Color(122,100,70)),
+	COPPER				("Copper", new String[]{"Copper ore"},true, true, new Color(74,48,32)),
+	TIN					("Tin", new String[]{"Tin ore"},true, true, new Color(106,106,106)),
+	LIMESTONE			("Limestone", new String[]{"Limestone"},true, true, new Color(110,110,110)),
+	BLUERITE			("Bluerite", new String[]{"Bluerite ore"},true, true, new Color(0,47,122)),
+	IRON				("Iron", new String[]{"Iron ore"},true, true, new Color(32,17,14)),
+	//ELEMENTAL			("Elemental", new String[]{"Elemental ore"}, true, true, new Color(0,0,0)),
+	//DAEYALT			("Daeyalt", new String[]{"Daeyalt ore"}, true, false),
+	SILVER				("Silver", new String[]{"Silver ore"},true, true, new Color(141,134,120), new Color(149,149,149)),
+	ESSENCE				("Essence", new String[]{"Rune essence", "Pure essence"},true, true),
+	COAL				("Coal", new String[]{"Coal"},true, true, new Color(24,24,17), new Color(0,0,0)),
+	PAY_DIRT			("Ore vein", new String[]{"Pay-dirt"},false, false),
+	SANDSTONE			("Sandstone", new String[]{"Sandstone (1kg)", "Sandstone (2kg)", "Sandstone (5kg)", "Sandstone (10kg)"},true, true, new Color(74,47,11)),
+	GEMS				("Gems", new String[]{"Uncut opal", "Uncut jade", "Uncut red topaz", "Uncut sapphire", "Uncut emerald", "Uncut diamond"},true, true, new Color(66,0,63)),
+	GOLD				("Gold", new String[]{"Gold ore"},true, true, new Color(0,0,0)),
+	GRANITE				("Granite", new String[]{"Granite (500g)", "Granite (2kg)", "Granite (5kg)"},true, true, new Color(94,66,40)),
+	MITHRIL				("Mithril", new String[]{"Mithril ore"},true, true, new Color(47,47,66)),
+	ADAMANTITE			("Adamantite", new String[]{"Adamantite ore"}, true, true, new Color(52,60,52)),
+	BANE				("Bane", new String[]{"Bane ore"}, true, false),
+	LIVING_ROCK_REMAINS	("Living rock remains", new String[]{"Living minerals"}, true, false),
+	CONCENTRATED_COAL	("Concentrated Coal", new String[]{"Coal"},	true, false),
+	CONCENTRATED_GOLD	("Concentrated Gold", new String[]{"Gold ore"}, true, false),
+	RUNITE				("Runite", new String[]{"Runite ore"}, true, true, new Color(73,98,102)),
+	SEREN				("Seren", new String[]{"Corrupted ore"}, true, false),
+	UNKNOWN				("Unknown", new String[]{}, false, false);
 
 	public String name;
 	public String[] oreNames;
@@ -64,7 +40,7 @@ public enum Rock {
 	public boolean osrs;
 	public Color[] colors;
 
-	Rock(String name, String[] oreNames, double[] exp, boolean rs3, boolean osrs){
+	Rock(String name, String[] oreNames, boolean rs3, boolean osrs){
 		this.name = name;
 		this.oreNames = oreNames;
 		this.rs3 = rs3;
@@ -72,7 +48,7 @@ public enum Rock {
 		colors = new Color[0];
 	}
 
-	Rock(String name, String[] oreNames, double[] exp, boolean rs3, boolean osrs, Color... colors){
+	Rock(String name, String[] oreNames, boolean rs3, boolean osrs, Color... colors){
 		this.name = name;
 		this.oreNames = oreNames;
 
