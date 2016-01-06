f26b0f417c3f8f522326adac9b8fe9ea976b0904
John Thomas <TheLimitInfinity@hotmail.com>
2016-01-06 16:18:23 -0500

    Added RS3 Barbarian Village to the list

--- src/scripts/mining/StandardMiner.java
+++ src/scripts/mining/StandardMiner.java
@@ -360,6 +360,7 @@ public class StandardMiner extends MiningStyle{
 		ArrayList<Location> locations = new ArrayList<Location>();
 		if(Environment.isRS3()){
 			locations.add(new scripts.mining.locations.rs3.AlKharid());
+			locations.add(new scripts.mining.locations.rs3.BarbarianVillage());
 			if(Environment.isSDK())locations.add(new scripts.mining.locations.rs3.CoalTrucks());
 			if(Environment.isSDK())locations.add(new scripts.mining.locations.rs3.DesertQuarry());
 			locations.add(new scripts.mining.locations.rs3.DwarvenMine());
